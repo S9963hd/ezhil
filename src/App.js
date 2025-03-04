5782430d2b9fd7a2a775aecd191ddb86e6381182
S9963hd <sanjaymajestic143@gmail.com>
2025-03-04 14:30:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="component1">
+        <div>
+              <img src="icon1.png"/>
+              <h1>Arrhythmia Disease Prediction</h1>
+          </div>
+          <p>fully AI integrated AI Website for arrhythmia prediction</p>
+      </div>
+      <input type="text" placeholder="Enter the points of ECG"/>
+    </div>
+  );
+}
+
+export default App;
